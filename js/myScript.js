e9861ac0354a4c3cb236a9e02a989121e4766ff5
Verdilac <lathindu2000@gmail.com>
2020-08-14 11:56:33 +0530

    button done if config to go

--- js/myScript.js
+++ js/myScript.js
@@ -1,9 +1,7 @@
-function changetext()
-{
-  document.getElementById("chgtext").innerHTML = "change text";
-}
+
 
 function loaddata()
 {
   document.getElementById("iphonexs").src = "images/xs2.jpg"
+  document.getElementById("chgtext").innerHTML = "change text";
 }
