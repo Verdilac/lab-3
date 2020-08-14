85e2be5f3db3381645a7ec8ece53ebe9122b1582
Verdilac <lathindu2000@gmail.com>
2020-08-14 10:20:37 +0530

    one button done

--- js/myScript.js
+++ js/myScript.js
@@ -2,3 +2,8 @@ function changetext()
 {
   document.getElementById("chgtext").innerHTML = "change text";
 }
+
+function loaddata()
+{
+  document.getElementById("iphonexs").src = "images/xs2.jpg"
+}
